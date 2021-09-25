c502db3189007235c29392b3ec40b3013a1a42ca
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-09-26 00:17:12 +0900

    add equatable config

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:random_command_game/app.dart';
 
 /// メイン関数
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  EquatableConfig.stringify = true;
   runApp(const App());
 }
