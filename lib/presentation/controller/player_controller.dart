a735776199fd35d1804795aa2c5f2501743574c3
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-09-26 03:49:28 +0900

    create controller

---
+++ lib/presentation/controller/player_controller.dart
@@ -0,0 +1,51 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:random_command_game/domain/entity/player.dart';
+
+final playerController =
+    StateNotifierProvider<PlayerController, AsyncValue<List<Player>>>(
+  (ref) => PlayerController(),
+);
+
+class PlayerController extends StateNotifier<AsyncValue<List<Player>>> {
+  PlayerController() : super(const AsyncLoading()) {
+    _fetch();
+  }
+
+  /// 同期
+  Future<void> _fetch() async {
+    final players = [
+      Player(id: '1', name: 'Player1', isSelected: true),
+      Player(id: '2', name: 'Player2', isSelected: true),
+      Player(id: '3', name: 'Player3', isSelected: true),
+    ];
+    state = AsyncData(players);
+  }
+
+  /// プレイヤー削除
+  Future<void> deletePlayer(String id) async {
+    final players = state.data!.value;
+    players.removeWhere((element) => element.id == id);
+    state = AsyncData(players);
+  }
+
+  /// プレイヤー作成
+  Future<void> createPlayer({required String name}) async {
+    final player = Player(id: 'name', name: name, isSelected: true);
+    final players = state.data!.value;
+    players.add(player);
+    state = AsyncData(players);
+  }
+
+  /// プレイヤー更新
+  Future<void> updatePlayer(String id, {bool? isSelected}) async {
+    final player = state.data!.value.singleWhere((element) => element.id == id);
+    final newPlayer = Player(
+        id: id, name: player.name, isSelected: isSelected ?? player.isSelected);
+    final players = state.data!.value;
+    final index = players.indexWhere((element) => element == player);
+    if (index != -1) {
+      players[index] = newPlayer;
+      state = AsyncData(players);
+    }
+  }
+}
