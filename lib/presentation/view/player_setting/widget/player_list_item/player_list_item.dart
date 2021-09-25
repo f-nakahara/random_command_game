f00f1579b7d75c6607d7fb2de90f69120ceeb2c9
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-09-26 02:31:59 +0900

    update player list item

--- lib/presentation/view/player_setting/widget/player_list_item/player_list_item.dart
+++ lib/presentation/view/player_setting/widget/player_list_item/player_list_item.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:random_command_game/presentation/view/common/check_box_card.dart';
+import 'package:random_command_game/presentation/view/player_setting/widget/player_list_item/player_list_item_view_model.dart';
 
-class PlayerListItem extends StatelessWidget {
+class PlayerListItem extends ConsumerWidget {
   const PlayerListItem({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final vm = ref.read(playerListItemViewModel.notifier);
+    final state = ref.watch(playerListItemViewModel);
     return CheckBoxCard(
-      title: 'こうだい',
-      onTap: (value) {},
-      onDelete: () {},
+      title: state.name,
+      onTap: (_) => vm.toggle(),
+      onDelete: vm.delete,
+      isSelected: state.isSelected,
     );
   }
 }
