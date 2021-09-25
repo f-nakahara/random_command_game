8d6eae379744b68d1ae56a4eab542b10a477fb5f
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-09-26 01:22:05 +0900

    add Sizer

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,28 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
-import 'package:random_command_game/presentation/view/home/home_page.dart';
+import 'package:random_command_game/presentation/view/player_setting/player_setting_page.dart';
+import 'package:sizer/sizer.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
+  static const themeMode = ThemeMode.system;
+  static const scheme = FlexScheme.aquaBlue;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Sizer(
+      builder: (context, orientation, deviceType) => MaterialApp(
+        title: 'Flutter Demo', // FIXME: タイトル変更
+        theme: FlexColorScheme.light(
+          scheme: scheme,
+        ).toTheme,
+        darkTheme: FlexColorScheme.dark(
+          scheme: scheme,
+        ).toTheme,
+        themeMode: themeMode,
+        home: const PlayerettingPage(),
       ),
-      home: const HomePage(),
     );
   }
 }
