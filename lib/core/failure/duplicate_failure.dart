1924615b4efed9a1e735db85fb34c67fbc58a86b
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-09-26 05:27:44 +0900

    create original error

---
+++ lib/core/failure/duplicate_failure.dart
@@ -0,0 +1,4 @@
+import 'package:random_command_game/core/failure/failure.dart';
+
+/// 重複
+class DuplicateFailure extends Failure {}
---
+++ lib/core/failure/not_found_failure.dart
@@ -0,0 +1,4 @@
+import 'package:random_command_game/core/failure/failure.dart';
+
+/// 該当するデータが存在しない
+class NotFoundFailure extends Failure {}
