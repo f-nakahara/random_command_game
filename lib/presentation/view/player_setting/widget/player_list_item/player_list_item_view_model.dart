83f3eb3078bdd1a801ece833c877861d6232e8a2
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-09-26 03:49:20 +0900

    update vm

--- lib/presentation/view/player_setting/widget/player_list_item/player_list_item_view_model.dart
+++ lib/presentation/view/player_setting/widget/player_list_item/player_list_item_view_model.dart
@@ -1,29 +1,37 @@
-import 'package:flutter/cupertino.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:random_command_game/domain/entity/player.dart';
+import 'package:random_command_game/presentation/controller/player_controller.dart';
 import 'package:random_command_game/presentation/view/player_setting/widget/player_list_item/player_list_item_state.dart';
 
-final playerListItemViewModel =
-    StateNotifierProvider<PlayerListItemViewModel, PlayerListItemState>(
-  (ref) => PlayerListItemViewModel(),
+final playerListItemViewModel = StateNotifierProvider.autoDispose
+    .family<PlayerListItemViewModel, PlayerListItemState, Player>(
+  (ref, player) => PlayerListItemViewModel(
+    player: player,
+    controller: ref.read(playerController.notifier),
+  ),
 );
 
 class PlayerListItemViewModel extends StateNotifier<PlayerListItemState> {
-  PlayerListItemViewModel()
-      : super(
-          PlayerListItemState.from(
-            const Player(id: 'id', name: 'こうだい', isSelected: false),
-          ),
+  PlayerListItemViewModel({
+    required this.player,
+    required PlayerController controller,
+  })  : _controller = controller,
+        super(
+          PlayerListItemState.from(player),
         );
 
+  final Player player;
+  final PlayerController _controller;
+
   /// 参加・不参加
-  void toggle() {
+  Future<void> toggle() async {
     final isSelected = !state.isSelected;
     state = state.copyWith(isSelected: isSelected);
+    await _controller.updatePlayer(player.id, isSelected: state.isSelected);
   }
 
   /// 削除
   Future<void> delete() async {
-    debugPrint('項目を削除します。');
+    await _controller.deletePlayer(player.id);
   }
 }
