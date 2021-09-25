a82a5ef44a79fd83890675f2a94ff9eac121d399
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-09-26 00:02:59 +0900

    update main

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:random_command_game/presentation/view/home/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
