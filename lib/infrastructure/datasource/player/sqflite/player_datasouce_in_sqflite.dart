276a78026f73e415bee2f4661b495d09ddc2702a
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-09-26 05:27:59 +0900

    handle try catch

--- lib/infrastructure/datasource/player/sqflite/player_datasouce_in_sqflite.dart
+++ lib/infrastructure/datasource/player/sqflite/player_datasouce_in_sqflite.dart
@@ -1,75 +1,109 @@
 import 'package:path/path.dart';
 import 'package:random_command_game/core/config/app_config.dart';
+import 'package:random_command_game/core/failure/duplicate_failure.dart';
+import 'package:random_command_game/core/failure/not_found_failure.dart';
 import 'package:random_command_game/domain/entity/player.dart';
 import 'package:random_command_game/infrastructure/datasource/player/i_player_datasource.dart';
 import 'package:random_command_game/infrastructure/datasource/player/sqflite/model/sql_player.dart';
 import 'package:sqflite/sqflite.dart';
 
 class PlayerDatasourceInSqflite implements IPlayerDatasource {
+  /// テーブル名
   static const _tableName = 'player';
 
   @override
   Future<SQLPlayer> find(String id) async {
-    final db = await _getDatabase();
-    final result = await db.query(
-      _tableName,
-      where: '${SQLPlayer.keyId}=?',
-      whereArgs: [id],
-    );
-    if (result.isEmpty) {
-      throw Exception();
+    try {
+      final db = await _getDatabase();
+      final result = await db.query(
+        _tableName,
+        where: '${SQLPlayer.keyId}=?',
+        whereArgs: [id],
+      );
+      if (result.isEmpty) {
+        throw NotFoundFailure();
+      }
+      final map = result.first;
+      final data = SQLPlayer.fromMap(map);
+      return data;
+    } catch (e) {
+      rethrow;
     }
-    final map = result.first;
-    final data = SQLPlayer.fromMap(map);
-    return data;
   }
 
   @override
   Future<List<SQLPlayer>> findAll() async {
-    final db = await _getDatabase();
-    final result = await db.query(_tableName, orderBy: SQLPlayer.keyCreatedAt);
-    if (result.isEmpty) {
-      throw Exception();
+    try {
+      final db = await _getDatabase();
+      final result =
+          await db.query(_tableName, orderBy: SQLPlayer.keyCreatedAt);
+      if (result.isEmpty) {
+        throw NotFoundFailure();
+      }
+      return result.map((e) => SQLPlayer.fromMap(e)).toList();
+    } catch (e) {
+      rethrow;
     }
-    return result.map((e) => SQLPlayer.fromMap(e)).toList();
   }
 
   @override
   Future<void> save(Player player) async {
-    final db = await _getDatabase();
-    final map = SQLPlayer.convertToMap(player);
-    await db.insert(_tableName, map);
+    try {
+      final db = await _getDatabase();
+      final result = await db.query(
+        _tableName,
+        where: '${SQLPlayer.keyId}=? OR ${SQLPlayer.keyName}=?',
+        whereArgs: [player.id, player.name],
+      );
+      if (result.isNotEmpty) {
+        throw DuplicateFailure();
+      }
+      final map = SQLPlayer.convertToMap(player);
+      await db.insert(_tableName, map);
+    } catch (e) {
+      rethrow;
+    }
   }
 
   @override
   Future<void> update(Player player) async {
-    final db = await _getDatabase();
-    final map = SQLPlayer.convertToMap(player);
-    await db.update(
-      _tableName,
-      map,
-      where: '${SQLPlayer.keyId}=?',
-      whereArgs: [player.id],
-    );
+    try {
+      final db = await _getDatabase();
+      final map = SQLPlayer.convertToMap(player);
+      await db.update(
+        _tableName,
+        map,
+        where: '${SQLPlayer.keyId}=?',
+        whereArgs: [player.id],
+      );
+    } catch (e) {
+      rethrow;
+    }
   }
 
   @override
   Future<void> delete(String id) async {
-    final db = await _getDatabase();
-    await db.delete(
-      _tableName,
-      where: '${SQLPlayer.keyId}=?',
-      whereArgs: [id],
-    );
+    try {
+      final db = await _getDatabase();
+      await db.delete(
+        _tableName,
+        where: '${SQLPlayer.keyId}=?',
+        whereArgs: [id],
+      );
+    } catch (e) {
+      rethrow;
+    }
   }
 
+  /// データベース取得
   Future<Database> _getDatabase() async {
-    final Database db = await openDatabase(
-      join(await getDatabasesPath(), AppConfig.playerDBFileName),
-      version: 1,
-      onCreate: (db, version) async {
-        await db.execute(
-          '''
+    try {
+      final Database db = await openDatabase(
+        join(await getDatabasesPath(), AppConfig.playerDBFileName),
+        version: 1,
+        onCreate: (db, version) async {
+          await db.execute(
+            '''
           CREATE TABLE $_tableName (
           ${SQLPlayer.keyId} TEXT PRIMARY KEY, 
           ${SQLPlayer.keyName} TEXT PRIMARY KEY, 
@@ -78,9 +112,12 @@ class PlayerDatasourceInSqflite implements IPlayerDatasource {
           ${SQLPlayer.keyUpdatedAt} INTEGER, 
           )
           ''',
-        );
-      },
-    );
-    return db;
+          );
+        },
+      );
+      return db;
+    } catch (e) {
+      rethrow;
+    }
   }
 }
