64ce6b15276a325a840d0709b76d42723c57fac9
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-09-26 03:48:03 +0900

    update widget

--- lib/presentation/view/player_setting/widget/player_list/player_list.dart
+++ lib/presentation/view/player_setting/widget/player_list/player_list.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:random_command_game/presentation/view/player_setting/widget/player_list/player_list_view_model.dart';
 import 'package:random_command_game/presentation/view/player_setting/widget/player_list_item/player_list_item.dart';
 
-class PlayerList extends StatelessWidget {
+class PlayerList extends ConsumerWidget {
   const PlayerList({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 20,
-      itemBuilder: (context, index) {
-        return const PlayerListItem();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncValue = ref.watch(playerListViewModel).players;
+    return asyncValue.when(
+      data: (data) {
+        if (data.isEmpty) {
+          return const SizedBox();
+        }
+
+        return ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (context, index) {
+            final player = data[index];
+            return PlayerListItem(player);
+          },
+        );
       },
+      loading: () => Container(),
+      error: (error, stack) => Container(),
     );
   }
 }
--- lib/presentation/view/player_setting/widget/player_list_item/player_list_item.dart
+++ lib/presentation/view/player_setting/widget/player_list_item/player_list_item.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:random_command_game/domain/entity/player.dart';
 import 'package:random_command_game/presentation/view/common/check_box_card.dart';
 import 'package:random_command_game/presentation/view/player_setting/widget/player_list_item/player_list_item_view_model.dart';
 
 class PlayerListItem extends ConsumerWidget {
-  const PlayerListItem({Key? key}) : super(key: key);
+  const PlayerListItem(this.player, {Key? key}) : super(key: key);
+
+  final Player player;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final vm = ref.read(playerListItemViewModel.notifier);
-    final state = ref.watch(playerListItemViewModel);
+    final vm = ref.read(playerListItemViewModel(player).notifier);
+    final state = ref.watch(playerListItemViewModel(player));
     return CheckBoxCard(
       title: state.name,
       onTap: (_) => vm.toggle(),
