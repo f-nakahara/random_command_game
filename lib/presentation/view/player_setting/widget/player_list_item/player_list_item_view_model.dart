c3fa9f0f3a76112647bc0843868dd0e4b0734bef
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-09-26 02:31:43 +0900

    create provider

--- lib/presentation/view/player_setting/widget/player_list_item/player_list_item_view_model.dart
+++ lib/presentation/view/player_setting/widget/player_list_item/player_list_item_view_model.dart
@@ -1,8 +1,20 @@
+import 'package:flutter/cupertino.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:random_command_game/domain/entity/player.dart';
 import 'package:random_command_game/presentation/view/player_setting/widget/player_list_item/player_list_item_state.dart';
 
+final playerListItemViewModel =
+    StateNotifierProvider<PlayerListItemViewModel, PlayerListItemState>(
+  (ref) => PlayerListItemViewModel(),
+);
+
 class PlayerListItemViewModel extends StateNotifier<PlayerListItemState> {
-  PlayerListItemViewModel(PlayerListItemState state) : super(state);
+  PlayerListItemViewModel()
+      : super(
+          PlayerListItemState.from(
+            const Player(id: 'id', name: 'こうだい', isSelected: false),
+          ),
+        );
 
   /// 参加・不参加
   void toggle() {
