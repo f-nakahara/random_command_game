2123c027de000512272be9ade596743883e8d203
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-09-27 04:04:02 +0900

    add create button

--- lib/presentation/view/player_setting/widget/player_create_button/player_create_button.dart
+++ lib/presentation/view/player_setting/widget/player_create_button/player_create_button.dart
@@ -1,37 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:random_command_game/core/l10n/app_localizations.dart';
-import 'package:random_command_game/presentation/view/common/text_form_dialog.dart';
-import 'package:random_command_game/presentation/view/player_setting/widget/player_create_button/player_create_button_view_model.dart';
+import 'package:random_command_game/presentation/view/common/create_button.dart';
+import 'package:random_command_game/presentation/view/player_setting/widget/player_create_form/player_create_form.dart';
 
-class PlayerCreateButton extends HookConsumerWidget {
+class PlayerCreateButton extends StatelessWidget {
   const PlayerCreateButton({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final vm = ref.read(playerCreateButtonViewModel);
-    final controller = useTextEditingController();
-    final localization = AppLocalizations.of(context)!;
-    return FloatingActionButton(
+  Widget build(BuildContext context) {
+    return CreateButton(
       onPressed: () {
         showDialog(
           context: context,
           builder: (context) {
-            return TextFormDialog(
-              onSaved: () async {
-                final name = controller.text;
-                await vm.create(name: name);
-              },
-              title: localization.createTitle(localization.player),
-              controller: controller,
-            );
+            return const PlayerCreateForm();
           },
         );
       },
-      child: const Icon(Icons.add),
     );
   }
 }
