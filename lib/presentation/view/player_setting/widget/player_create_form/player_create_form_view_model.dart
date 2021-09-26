09fab90e3f3670a42d5c3b5b817d2dbafd201aa1
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-09-27 04:03:48 +0900

    move file and rename

--- lib/presentation/view/player_setting/widget/player_create_form/player_create_form_view_model.dart
+++ lib/presentation/view/player_setting/widget/player_create_form/player_create_form_view_model.dart
@@ -1,14 +1,15 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:random_command_game/presentation/controller/player_controller.dart';
 
-final playerCreateButtonViewModel = Provider<PlayerCreateButtonViewModel>(
-  (ref) => PlayerCreateButtonViewModel(
+final playerCreateFormViewModel =
+    Provider.autoDispose<PlayerCreateFormViewModel>(
+  (ref) => PlayerCreateFormViewModel(
     ref.read(playerController.notifier),
   ),
 );
 
-class PlayerCreateButtonViewModel {
-  PlayerCreateButtonViewModel(this.controller);
+class PlayerCreateFormViewModel {
+  PlayerCreateFormViewModel(this.controller);
 
   final PlayerController controller;
 
