f6d61721ce150b63368ae5b3b55af79916398334
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-09-28 01:51:28 +0900

    create target controller

---
+++ lib/presentation/controller/target_controller.dart
@@ -0,0 +1,27 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:random_command_game/application/target/target_application.dart';
+import 'package:random_command_game/application/target/target_application_provider.dart';
+import 'package:random_command_game/domain/entity/target.dart';
+
+final targetController =
+    StateNotifierProvider<TargetController, AsyncValue<List<Target>>>(
+  (ref) => TargetController(
+    app: ref.read(targetApplicationProvider),
+  ),
+);
+
+class TargetController extends StateNotifier<AsyncValue<List<Target>>> {
+  TargetController({required TargetApplication app})
+      : _app = app,
+        super(const AsyncLoading()) {
+    _fetch();
+  }
+
+  final TargetApplication _app;
+
+  /// 同期
+  Future<void> _fetch() async {
+    final players = await _app.getAllPlayerList();
+    state = AsyncData(players);
+  }
+}
