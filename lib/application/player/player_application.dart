d27a6ff8cb0defdb5780948c66317f5e802d70ec
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-09-27 19:50:07 +0900

    add method and update

--- lib/application/player/player_application.dart
+++ lib/application/player/player_application.dart
@@ -1,3 +1,4 @@
+import 'package:random_command_game/core/failure/duplicate_failure.dart';
 import 'package:random_command_game/domain/entity/player.dart';
 import 'package:random_command_game/domain/repository/i_player_repository.dart';
 import 'package:uuid/uuid.dart';
@@ -15,22 +16,47 @@ class PlayerApplication {
 
   /// プレイヤーを作成する
   Future<Player> createPlayer({required String name}) async {
-    final id = const Uuid().v4();
-    final date = DateTime.now();
-    final player = Player(
+    final isDuplicated = await _isDuplicated(name: name);
+    if (isDuplicated) {
+      throw DuplicateFailure();
+    } else {
+      final id = const Uuid().v4();
+      final date = DateTime.now();
+      final player = Player(
+        id: id,
+        name: name,
+        isSelected: true,
+        createdAt: date,
+        updatedAt: date,
+      );
+      await _repository.save(player);
+      final result = await _repository.find(id);
+      return result;
+    }
+  }
+
+  /// プレイヤーを更新する
+  Future<Player> updatePlayer(String id, {required bool? isSelected}) async {
+    final oldPlayer = await _repository.find(id);
+    final newPlayer = Player(
       id: id,
-      name: name,
-      isSelected: true,
-      createdAt: date,
-      updatedAt: date,
+      name: oldPlayer.name,
+      isSelected: isSelected ?? oldPlayer.isSelected,
+      createdAt: oldPlayer.createdAt,
+      updatedAt: oldPlayer.updatedAt,
     );
-    await _repository.save(player);
-    final result = await _repository.find(id);
-    return result;
+    await _repository.update(newPlayer);
+    return newPlayer;
   }
 
   /// プレイヤーを削除する
   Future<void> deletePlayer(String id) async {
     await _repository.delete(id);
+  }
+
+  /// 同じ名前のプレイヤーが既に存在しているか確認
+  Future<bool> _isDuplicated({required String name}) async {
+    final player = await _repository.findByName(name);
+    return player != null;
   }
 }
